8bbef4e61b002ad96f60fa90d1956db3e900b4d4
abhilash <abhilashpal128@gmail.com>
2024-07-11 12:07:02 +0530

    new push

--- utils/apis/TeacherApis/login.js
+++ utils/apis/TeacherApis/login.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
-const apiUrl = "http://192.168.1.131:5000/v1";
+// const apiUrl = "http://192.168.1.131:5000/v1";
 // const apiUrl = "http://192.168.68.120:5000/v1";
-// const apiUrl = "http://192.46.208.52:5000/v1";
+const apiUrl = "http://192.46.208.52:5000/v1";
+
 const config = {
   headers: {
     "Content-Type": "application/json",
